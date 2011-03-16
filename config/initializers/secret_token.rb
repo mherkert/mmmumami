4d073dce950892d3761dcdef218cd4ac637d55b9
Marion Herkert <marion.herkert@gmx.de>
2011-03-16 22:20:51 +0000

    Initial commit woohoo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mmmumami::Application.config.secret_token = '4497938a77621270dd50681bfbfa340ce9a5a520464c928b8506d6bfc9d067eca8dbee59d1a04e049d2d480efc7883eff41afab7a8fd8089f37b9964b1ace514'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mmmumami::Application.config.session_store :cookie_store, :key => '_mmmumami_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mmmumami::Application.config.session_store :active_record_store
