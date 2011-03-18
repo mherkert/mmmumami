89498b1a09043ce3e520b4ee80c81c7cd6749183
Marion Herkert <marion.herkert@gmx.de>
2011-03-18 12:46:18 +0000

    link tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   
+  def logo
+     image_tag("logo.png", :alt => "MmmUmami", :class => "round")
+  end
+  
   # Return a title on a per-page basis.
   def title
     base_title = "MmmUmami" 
