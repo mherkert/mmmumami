e5a0631642d7312f6da3a73a77bfab18e04a2de7
Marion Herkert <marion.herkert@gmx.de>
2011-03-20 11:18:23 +0000

    updating and viewing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  
+  def index
+    @users = User.paginate(:page => params[:page])
+    @title = "All users"
+  end
+  
   def new
     @user = User.new
     @title = "Sign up"
@@ -21,4 +29,30 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit    
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile successfully updated!"
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  private
+    
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
